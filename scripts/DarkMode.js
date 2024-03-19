99f97593aa12911e6f6a9cd37e02aa0f11b7a698
ratibsami <137919504+ratibsami@users.noreply.github.com>
2024-03-19 19:27:20 +0430

    Modified

--- scripts/DarkMode.js
+++ scripts/DarkMode.js
@@ -1,28 +1,29 @@
-//DarkMode Style DesignXperience ©️ Copyright all rights
 const toggleDarkMode = () => {
     const body = document.body;
 
     body.classList.toggle("dark-mode");
+    const isDarkMode = body.classList.contains("dark-mode");
+    localStorage.setItem("darkMode", isDarkMode ? "true" : "false");
 };
 
 const updateMode = () => {
     const body = document.body;
-    const isDarkModePreferred = 
-    window.matchMedia("prefers-color-scheme: dark").matches;
 
+
+    const isDarkModePreferred = localStorage.getItem("darkMode") === "true";
+
+ 
     if (isDarkModePreferred) {
         body.classList.add("dark-mode");
-    }  else {
+    } else {
+        body.classList.remove("dark-mode");
+    }
+};
 
-            body.classList.remove("dark-mode");
-        }
-
-    
-}
-
-const button =
-document.getElementById("dark-mode-toggle");
+const button = document.getElementById("dark-mode-toggle");
 button.addEventListener("click", toggleDarkMode);
 
 document.addEventListener("DOMContentLoaded", updateMode);
 
+
+window.addEventListener("popstate", updateMode);
